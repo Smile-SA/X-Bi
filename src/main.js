523a2183ccc14a765a2e39f8d9e53d5179e8f944
Valentin Daviot <valentin.daviot@alterway.fr>
2019-09-10 11:13:33 +0200

    added views
    Signed-off-by: Valentin Daviot <valentin.daviot@alterway.fr>

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueRangedatePicker from 'vue-rangedate-picker'
+import VueContext from 'vue-context'
+
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+// eslint-disable-next-line no-unexpected-multiline
+(async () => {
+  Vue.component('VueRangedatePicker', VueRangedatePicker)
+  Vue.component('VueContext', VueContext)
+  
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+})()
+  
--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
-import DashLayout from './components/DashLayout.vue'
-import HomeView from './components/Home.vue'
-import NodesView from './components/Nodes.vue'
-import NamespacesView from './components/Namespaces.vue'
-import PodsView from './components/Pods.vue'
+import HomeView from './views/Home.vue'
+import NodesView from './views/Nodes.vue'
+import NamespacesView from './views/Namespaces.vue'
+import PodsView from './views/Pods.vue'
 
 Vue.use(Router)
 
@@ -16,15 +14,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'dash',
-      component: DashLayout,
+      name: 'Overall',
+      meta: { description: 'Global infrastructure view', requiresAuth: false },
+      component: HomeView,
       children: [
-        {
-          path: 'home',
-          component: HomeView,
-          name: 'Overall',
-          meta: { description: 'Global infrastructure view', requiresAuth: false }
-        },
         {
           path: 'namespaces',
           component: NamespacesView,
