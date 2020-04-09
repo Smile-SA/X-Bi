352e1e106bed3d093f07f8d242824d44d703aa43
Ben Hamed <houssemeddine.ben-hamed@alterway.fr>
2020-04-09 10:27:39 +0200

    [storybook] knobs for contact elements (name and email)

---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+// .storybook/preview.js
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'highlight.js/styles/default.css';
---
+++ src/components/partials/ContactLabel.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="pull-left info">
+     <div>
+       <p class="white">{{ displayName }}</p>
+      </div>
+      <span class="text-muted">{{ email }}</span>
+   </div>
+</template>
+<script>
+    export default {
+        name: 'ContactLabel',
+        props: ['displayName', 'email'],
+    }
+</script>
