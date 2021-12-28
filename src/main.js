09aa3213446d9df3172db4c5471b2682579dfe76
koku-ulrich.gblokpo <koku-ulrich.gblokpo@alterway.fr>
2021-12-28 15:09:18 +0100

    fix url loading bugs

--- src/main.js
+++ src/main.js
@@ -1,25 +1,22 @@
 import Vue from 'vue'
-import App from './app/index.vue'
-import router from './router'
-import VueRangedatePicker from 'vue-rangedate-picker'
-import { VueContext } from 'vue-context'
-import VueHighlightJS from 'vue-highlight.js';
-import yaml_highlight from 'highlight.js/lib/languages/yaml.js';
-import jsyaml from 'js-yaml'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-Vue.use(jsyaml)
-Vue.use(VueHighlightJS, {languages: {yaml_highlight}})
-Vue.config.productionTip = false
-Vue.component('VueContext', VueContext)
-Vue.component('VueRangedatePicker', VueRangedatePicker)
-window.jQuery = require('jquery')
-window.$ = require('jquery')
-// eslint-disable-next-line no-unexpected-multiline
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App),
-  components: { }
-});
 
+import router from './router';
+import AppComponent from './app/index.vue';
+import VueRangedatePicker from 'vue-rangedate-picker';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/eclipse.css';
+import "../public/css/bootstrap.min.css";
+import "../public/font-awesome/css/font-awesome.css";
+import "../public/js/plugins/gritter/jquery.gritter.css";
+import "../public/css/plugins/toastr/toastr.min.css";
+import "../public/css/animate.css";
+import "../public/css/style.css";
+import 'codemirror/mode/javascript/javascript.js';
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+Vue.component('VueRangedatePicker', VueRangedatePicker);
+new Vue({
+  router,
+  render: h => h(AppComponent)
+}).$mount('#app');
