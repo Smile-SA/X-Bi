53a5d726c7f5fdd2b9961599efed617357421009
koku-ulrich.gblokpo <koku-ulrich.gblokpo@alterway.fr>
2022-06-16 11:27:36 +0200

    add avg function

--- src/components/graphics/card/card.js
+++ src/components/graphics/card/card.js
@@ -12,7 +12,7 @@ export default {
         return {
             timer: '',
             value: '-',
-            lstm:{}
+            lstm: {}
         }
     },
     methods: {
@@ -23,23 +23,27 @@ export default {
                 case"default" :
                 case"multi-icon":
                     this.lstm = {}
-                    await general.getJsonData(this.queryBegin + this.configuration.query + this.setQuery,this.configuration.method).then((r) => {
+                    await general.getJsonData(this.queryBegin + this.configuration.query + this.setQuery, this.configuration.method).then((r) => {
                         if (r.total > 0) {
                             switch (this.configuration.method) {
+                                case "avg":
+                                    this.configuration.value = (r.results.map(item => item[this.configuration.query_key])
+                                        .reduce((a, b) => a + b) / r.results.length)
+                                        .toFixed(2)
+                                    break;
                                 case "count":
                                     this.configuration.value = r.total;
+                                    break;
+                                case "lstm":
+                                    this.configuration.value = (r.results[this.configuration.query_key]).toFixed(2)
+                                    this.configuration.icon = 'fa-arrow-' + (r.results.trend).toLowerCase()
+                                    this.lstm = r.results
                                     break;
                                 case "sum":
                                     this.configuration.value = (r.results.length >= 1) ?
                                         r.results[0][this.configuration.query_key].toFixed(2) :
                                         r.results[0].map(item => item[this.configuration.query_key]).reduce((a, b) => a + b, 0).toFixed(2)
                                     break;
-                                case "lstm":
-                                    this.configuration.value = (r.results[this.configuration.query_key]).toFixed(2)
-                                    this.configuration.icon = 'fa-arrow-'+(r.results.trend).toLowerCase()
-                                    this.lstm = r.results
-                                    break;
-
                             }
                             this.$forceUpdate()
                         } else {
@@ -53,36 +57,13 @@ export default {
         },
 
         async setQueryData() {
-            if(this.configuration.method!='lstm'){
+            if (this.configuration.method != 'lstm') {
                 this.setQuery = await utils.convertURLDateParameter(this.from, this.to)
-            }else this.setQuery="";
+            } else this.setQuery = "";
             return this.setQuery;
         },
 
-        // cardFetch() {
-        //   const queryDate = utils.convertURLDateParameter(this.configuration.from, this.configuration.to)
-        //   return fetch(this.url + queryDate, {credentials: 'include'}).then(response => response.json())
-        // },
-        // fetchString() {
-        //   this.cardFetch().then(r => this.value = r.results[0][this.configuration.key])
-        //   this.$forceUpdate()
-        // },
-        // fetchSum() {
-        //   this.cardFetch().then(r => {
-        //     this.value = (r.results.length === 1) ?
-        //         r.results[0][this.configuration.key].toFixed(2) :
-        //         r.results.map(item => item[this.configuration.key]).reduce((a, b) => a + b, 0).toFixed(2)
-        //     this.$forceUpdate()
-        //   })
-        // },
-        // fetchAverrage() {
-        //   this.cardFetch().then(r => {
-        //     this.value = (r.results.map(item => item[this.configuration.key])
-        //         .reduce((a, b) => a + b) / r.results.length)
-        //         .toFixed(2)
-        //     this.$forceUpdate()
-        //   })
-        // },
+
     },
     beforeMount() {
         this.getData(this.configuration.method);
